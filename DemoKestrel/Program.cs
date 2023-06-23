8981b756e8382d1fb9aadc854874b91619b01625
Andrei Errapart <andrei.errapart@trenz.local>
2023-06-23 07:37:06 +0200

    DemoKestrel: First application.

---
+++ DemoKestrel/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoKestrel
+{
+    // Sample app from: https://github.com/dotnet/aspnetcore/tree/main/src/Servers/Kestrel/samples/SampleApp
+    public class Program
+    {
+        public static async void Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args);
+            //     "windowsAuthentication": true,
+            // "anonymousAuthentication": true,
+            builder.UseConsoleLifetime(); // https://stackoverflow.com/questions/64837558/how-to-cancel-webhost-when-process-is-stopping
+
+            var r = builder.ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("https://0.0.0.0:4444");
+                    webBuilder.UseStartup<Startup>();
+                });
+            return r;
+
+        }
+    }
+}
---
+++ DemoKestrel/README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+Just a demo of embedding Kestrel and some services into a console app.
+
+
